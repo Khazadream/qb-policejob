4f0a1f910dec6ba52211c1dbcdf80f1d3000b77c
Khazadream <97912361+Khazadream@users.noreply.github.com>
2025-09-02 18:18:35 +0200

    add a notif on valid scan

--- cl_khaza/fingerprint.lua
+++ cl_khaza/fingerprint.lua
@@ -1,3 +1,4 @@
 RegisterNetEvent("qb-policejob:client:registerFingerPrint", function(citizenID, fingerPrint)
     TriggerServerEvent("evange-core:server:mdt:set-fingerPrint", citizenID, fingerPrint)
+    exports['qb-core']:Notify('Empreintes digitales enregistré avec succès.', 'success')
 end)
